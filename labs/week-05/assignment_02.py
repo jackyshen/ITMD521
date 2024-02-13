5f767a01010a98d04291d20e2b3e84f26c8d40e4
Jie Shen <shenjie7810@gmail.com>
2024-02-12 22:40:25 -0600

    init

---
+++ labs/week-05/assignment_02.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+
+import sys
+
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: mnmcount <file>", file=sys.stderr)
+        sys.exit(-1)
+
+    spark = (SparkSession
+        .builder
+        .appName("PythonMnMCount")
+        .getOrCreate())
+    # get the M&M data set file name
+    mnm_file = sys.argv[1]
+    # read the file into a Spark DataFrame
+    mnm_df = (spark.read.format("csv")
+        .option("header", "true")
+        .option("inferSchema", "true")
+        .load(mnm_file))
+    mnm_df.show(n=5, truncate=False)
