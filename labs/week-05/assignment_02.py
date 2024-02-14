e7dc0bf341c794b1cddb07520df2f2370f01d3f0
Jie Shen <shenjie7810@gmail.com>
2024-02-13 23:17:00 -0600

    submit

--- labs/week-05/assignment_02.py
+++ labs/week-05/assignment_02.py
@@ -1,10 +1,16 @@
-
 from __future__ import print_function
 import sys
 from pyspark.sql import SparkSession
-
-
-
+from pyspark.sql.functions import to_timestamp
+from pyspark.sql.functions import col
+from pyspark.sql import *
+from pyspark.sql.functions import year
+from pyspark.sql.functions import month
+from pyspark.sql.functions import count
+from pyspark.sql.functions import first
+from pyspark.sql.functions import avg
+from pyspark.sql.functions import weekofyear
+from pyspark.sql.functions import corr
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: assignment_02 <file>", file=sys.stderr)
@@ -20,11 +26,44 @@ if __name__ == "__main__":
     df = (spark.read.format("csv")
         .option("header", "true")
         .option("inferSchema", "true")
-        .load(mnm_file))
+        .load(csv_file))
     df.show(10)
     df.printSchema()
-    
+    fire_new_df = (df.withColumn("NewCallDate", to_timestamp(col("CallDate"),"MM/dd/yyyy")).drop("CallDate"))
+    fire_new_df.select("CallType").filter(year("NewCallDate")==2018).distinct().show(truncate=True)
+    '''file_df = fire_new_df.select("CallType").filter(year("NewCallDate")==2018).distinct().show(truncate=True)
+    '''
+    ''' try parquet files '''
+    fire_new_df.write.mode('overwrite').parquet("./q1.txt")
+    file_parquet=spark.read.parquet("./q1.txt")
+    file_parquet.show();
 
-
-
+    ''' try db '''
+    spark.sql("CREATE DATABASE IF NOT EXISTS my_database")
+    fire_new_df.filter(year("NewCallDate")==2018) \
+            .groupBy(month("NewCallDate").alias("month")) \
+            .agg(count("*").alias("count")) \
+            .orderBy(col("count").desc()).select("month","count").show(1,truncate=True)
+    fire_new_df.filter(year("NewCallDate")==2018)\
+            .groupBy("Neighborhood")\
+            .agg(count("*").alias("count"))\
+            .orderBy(col("count").desc()).select("Neighborhood","count").show(1,truncate=True)
+    fire_new_df.filter(year("NewCallDate")==2018)\
+            .groupBy("Neighborhood")\
+            .agg(avg("Delay").alias("avg_delay"))\
+            .orderBy(col("avg_delay").desc()).select("Neighborhood","avg_delay").show(1,truncate=True)
+    fire_new_df.filter(year("NewCallDate")==2018)\
+            .groupBy(weekofyear("NewCallDate").alias("week"))\
+            .agg(count("*").alias("count"))\
+            .orderBy(col("count").desc()).select("week","count").show(1,truncate=True)
+    neighborhood_zip_counts = fire_new_df.groupBy("Zipcode").agg(count("*").alias("fire_calls_count"))
+    correlation_value = neighborhood_zip_counts.stat.corr("Zipcode", "fire_calls_count")
+    print("Correlation between neighborhood and number of fire calls:", correlation_value)
     spark.stop()
+'''    properties = {
+            "user":"pyspark",
+            "password":"pyspark",
+            "driver":"org.mariadb.jdbc.Driver"
+    }
+    jdbc_url="jdbc:mysql://127.0.0.1:3306/itmd521"
+    fire_new_df.write.jdbc(url=jdbc_url,table="py_csv",mode = "overwrite",properties=properties)'''
---
+++ labs/week-05/src/main/scala/assignment-01.scala
@@ -0,0 +1,97 @@
+package main.scala
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions.{col, expr}
+
+object Lab2{
+def main(args: Array[String]) {
+
+// 创建一个 SparkSession
+val spark = SparkSession.builder
+  .appName("Lab2")
+  .getOrCreate()
+
+
+// 读取数据源创建 DataFrame
+val df = spark.read
+  .format("csv")
+  .option("header", "true")
+  .option("inferSchema", "true")
+  .load("./data/Divvy_Trips_2015-Q1.csv")
+
+
+df.show()
+df.printSchema()
+
+// 打印 DataFrame 的 Schema
+val schema = StructType(Array(
+      StructField("trip_id", StringType, true),
+      StructField("starttime", StringType, true),
+      StructField("stoptime", StringType, true),
+      StructField("bikeid", StringType, true),
+      StructField("tripduration", StringType, true),
+      StructField("from_station_id", StringType, true),
+      StructField("from_station_name", StringType, true),
+      StructField("to_station_id", StringType, true),
+      StructField("to_station_name", StringType, true),
+      StructField("usertype", StringType, true),
+      StructField("gender", StringType, true),
+      StructField("birthyear", StringType, true)
+    ))
+
+    // Read the CSV file into a DataFrame with the specified schema
+    val df_with_schema = spark.read
+      .format("csv")
+      .option("header", "true")
+      .schema(schema)
+      .load("./data/Divvy_Trips_2015-Q1.csv")
+
+    // Show the DataFrame
+    df_with_schema.show()
+
+    // Print the number of rows
+    println("\nNumber of Rows:", df_with_schema.count())
+
+  val ddlSchema =
+      """
+        |trip_id STRING,
+        |starttime STRING,
+        |stoptime STRING,
+        |bikeid STRING,
+        |tripduration STRING,
+        |from_station_id STRING,
+        |from_station_name STRING,
+        |to_station_id STRING,
+        |to_station_name STRING,
+        |usertype STRING,
+        |gender STRING,
+        |birthyear STRING
+      """.stripMargin
+
+    // Read the CSV file into a DataFrame with the specified schema
+    val df_ddl = spark.read
+      .format("csv")
+      .option("header", "true")
+      .option("inferSchema", "false")
+      .option("delimiter", ",")
+      .option("schema", ddlSchema)
+      .load("./data/Divvy_Trips_2015-Q1.csv")
+
+      
+    df_ddl.show()
+
+    df_ddl.select("*").where(col("gender")==="Male").groupBy("to_station_name").count().show(10)
+  /*  val formatDf = df_ddl.select("*")
+      .where(col("gender") === "Male")
+      .groupBy("to_station_name")
+     .agg(count("to_station_name").alias("station_count"))
+// Show the first 10 rows of the resulting DataFrame
+      formatDf.show(10)
+*/
+    // Stop the SparkSession
+    spark.stop()
+
+}
+}
+
