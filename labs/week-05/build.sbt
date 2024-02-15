4ff64957ebe0acf08af1c7d11d6bbb70b6d959e0
Jie Shen <shenjie7810@gmail.com>
2024-02-14 22:31:18 -0600

    commit

--- labs/week-05/build.sbt
+++ labs/week-05/build.sbt
@@ -4,9 +4,8 @@ name := "main/scala"
 version := "1.0"
 //version of Scala
 scalaVersion := "2.12.10"
-// spark library dependencies 
+// spark library dependencies
 // change this to 3.0.0 when released
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "3.0.0-preview2",
   "org.apache.spark" %% "spark-sql"  % "3.0.0-preview2"
-)
--- labs/week-05/src/main/scala/assignment_02.scala
+++ labs/week-05/src/main/scala/assignment_02.scala
@@ -50,9 +50,7 @@ timestamp: Long)
     //q2
     df_with_schema.select("cn").where(col("c02_level") < 1000).show(false)
     //q3
-   // var minMaxRow=df_with_schema.select(min("temp").alias("min_temp"),max("temp").alias("max_temp"),min("battery_le
-vel").alias("min_battery"),max("battery_level").alias("max_battery"),min("c02_level").alias("min_c02"),max("c02_level
-").alias("max_c02"),min("humidity").alias("min_humidity"),max("humidity").alias("max_humidity"))
+   // var minMaxRow=df_with_schema.select(min("temp").alias("min_temp"),max("temp").alias("max_temp"),min("battery_level").alias("min_battery"),max("battery_level").alias("max_battery"),min("c02_level").alias("min_c02"),max("c02_level").alias("max_c02"),min("humidity").alias("min_humidity"),max("humidity").alias("max_humidity"))
     //var minMaxRow=df_with_schema.select(min("temp").alias("min_temp"),max("temp").alias("max_temp"))
      //val minValue = minMaxRow.get[Int]("min_temp")
     //val maxValue = minMaxRow.get[Int]("max_temp")
@@ -61,11 +59,11 @@ vel").alias("min_battery"),max("battery_level").alias("max_battery"),min("c02_le
     println("min temp is :")
      df_with_schema.select("temp").agg(min("temp")).show(false)
     println("max temp is :")
-    df_with_schema.select("temp").agg(max("temp")).show(false)
+    df_with_schema.select("temp").agg(max("temp")).show(false)  
  println("min battery level is :")
      df_with_schema.select("battery_level").agg(min("battery_level")).show(false)
     println("max battery level is :")
-    df_with_schema.select("battery_level").agg(max("battery_level")).show(false)
+    df_with_schema.select("battery_level").agg(max("battery_level")).show(false)    
  println("min c02 level is :")
      df_with_schema.select("c02_level").agg(min("c02_level")).show(false)
     println("max c02 level is :")
@@ -75,7 +73,7 @@ vel").alias("min_battery"),max("battery_level").alias("max_battery"),min("c02_le
     println("max humidity is :")
     df_with_schema.select("humidity").agg(max("humidity")).show(false)
     //q4
-
+    
     val result :DataFrame = df_with_schema.groupBy("cn").agg(avg("temp").alias("avg_temp"),
       avg("c02_level").alias("avg_c02"),
       avg("humidity").alias("avg_humidity"))
@@ -85,3 +83,5 @@ vel").alias("min_battery"),max("battery_level").alias("max_battery"),min("c02_le
     spark.stop()
 }
 }
+
+
